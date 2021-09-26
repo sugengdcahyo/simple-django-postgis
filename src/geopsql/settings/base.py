c2dc7b8fb9a4bdf0079e65692a0f9d46189ebb0c
Sugeng D. Cahyo <sugengdcahyo@gmail.com>
2021-09-27 01:44:48 +0700

    custom user model

--- src/geopsql/settings/base.py
+++ src/geopsql/settings/base.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party
+
+    # own
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.user'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
